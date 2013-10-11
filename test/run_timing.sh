54bebc0e45a111d05405364a7fa97cd0d0e5b7b7
Hammad Mazhar <hammad.maz@gmail.com>
2013-10-11 16:14:29 -0500

    Added a simple timing script

---
+++ test/run_timing.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+for t in {2..8}
+do
+for a in {0..1}
+do
+for b in {0..1}
+do
+for c in {0..1}
+do
+for d in {0..1}
+do
+for e in {0..1}
+do
+for f in {0..1}
+do
+for g in {0..1}
+do
+for h in {0..1}
+do
+for i in {0..1}
+do
+for j in {0..1}
+do
+
+./timing_Broadphase $a $b $c $d $e $f $g $h $i $j $t
+
+
+done
+
+done
+done
+done
+done
+done
+done
+done
+done
+done
+
+
+done
